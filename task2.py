1f1509d6049b494e0e21cdcf2ef5c1bf921f304b
Anvar.Dautov <anv_99@mail.ru>
2022-06-01 21:06:32 +0300

    create readme.md files

--- task2.py
+++ task2.py
@@ -1,15 +1,24 @@
-# Создать и заполнить файл случайными целыми значениями. Выполнить сортировку содержимого
-# файла по возрастанию.
+# Создать и заполнить файл случайными целыми значениями. 
+# Выполнить сортировку содержимого файла по возрастанию. 
 
-max_val = 100
-quantity = 10
-data = open(path, 'w')
- for i in range(quantity):
-     data.write(f'{str(randint(0, max_val))},')
- data.close()
+from random import randint
 
- with open(path, 'r') as fr:
-     list_int = fr.readline().split(',')
-     list_int.pop()
-     list_int.sort(key=(int))
- print(list_int)
+my_filename = 'data_task02.txt'
+
+my_list = [randint(0,100) for _ in range(randint(0,100))]
+
+print(my_list)
+
+with open(my_filename,'w') as f:
+    f.write("\n".join(map(str,my_list)))
+
+my_list = []
+with open(my_filename,'r') as f:
+    my_list = f.readlines()
+    my_list = list(map(int, my_list))
+    my_list.sort()
+
+with open(my_filename,'w') as f:
+    f.write("\n".join(map(str,my_list)))
+
+    print(my_list)
